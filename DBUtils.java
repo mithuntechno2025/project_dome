3340eaf7f6010011b588222cdaf19df267ea89c6
rahul <rahulckhairnar@gmail.com>
2025-09-28 04:26:50 +0000

    Updating some chnages in ext file & creates new script

--- DBUtils.java
+++ DBUtils.java
@@ -1,4 +1,5 @@
 This files contains DBUtils files
 Added some more content regarding this utility
 Modifying some changes as per the client requirnment
+Adding the script on dated 28th Sept
 
---
+++ python.py
@@ -0,0 +1,2 @@
+This is python related script file
+
